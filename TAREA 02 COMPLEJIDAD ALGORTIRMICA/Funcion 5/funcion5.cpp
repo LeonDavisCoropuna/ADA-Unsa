a0a0ad6bc6a115f700b2d525c25f513e9849dc13
Leon Davis <104030173+LeonDavisCoropuna@users.noreply.github.com>
2023-06-25 22:05:14 -0500

    Create funcion5.cpp

---
+++ TAREA 02 COMPLEJIDAD ALGORTIRMICA/Funcion 5/funcion5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+void funcion5(string _a,string _b){
+    string A = _a, B = _b; 
+    int a=0,b=0,c=0;
+    for(int i=0;i <= A.size()+2;i++){ // A + 1
+        for(int j=0;j <= B.size()+1;j++){ // A(B+1)
+            c++;
+            cout<<A[i]<<","<<B[j]<<endl; // AB
+        }
+    }
+    cout<<a<<"-"<<b<<"-"<<c<<endl;
+}
+
+
+int main(){
+    funcion5("hola","buenas");
+    return 0;
+}
